31dea560792d92f7398f733ea034ee0a85be596a
Michel Weststrate <mweststrate@gmail.com>
2016-08-13 03:53:39 +0200

    Documented from promise

--- src/from-promise.ts
+++ src/from-promise.ts
@@ -38,11 +38,36 @@ class PromiseBasedObservable<T> implements IPromiseBasedObservable<T> {
 }
 
 /**
+ * `fromPromise` takes a Promise and returns an object with 3 observable properties that track
+ * the status of the promise. The returned object has the following observable properties:
+ * `value`: either the initial value, or the value the Promise resolved to
+ * `state`: one of `"pending"`, `"fulfilled"` or `"rejected"`
+ * `reason`: the reject reason if the state is `"rejected"`
+ * `promise`: (not observable) the original promise object
  *
+ * @example
+ * const fetchResult = fromPromise(fetch("http://someurl"))
  *
- * @param {IThenable<T>} promise
- * @param {T} [initialValue=undefined]
- * @param {any} [modifier=IDENTITY]
+ * // combine with when..
+ * when(
+ *   () => fetchResult.state !== "pending"
+ *   () => {
+ *     console.log("Got ", fetchResult.reason || fetchResult.value)
+ *   }
+ * )
+ *
+ * // or a mobx-react component..
+ * const myComponent = observer(({ fetchResult }) => {
+ *   switch(fetchResult.state) {
+ *      case "pending": return <div>Loading...</div>
+ *      case "rejected": return <div>Ooops... {fetchResult.reason}</div>
+ *      case "fulfilled": return <div>Gotcha: {fetchResult.value}</div>
+ *   }
+ * })
+ *
+ * @param {IThenable<T>} promise The promise which will be observed
+ * @param {T} [initialValue=undefined] Optional predefined initial value
+ * @param {any} [modifier=IDENTITY] MobX modifier, e.g. `asFlat`, to be applied to the resolved value
  * @returns {IPromiseBasedObservable<T>}
  */
 export function fromPromise<T>(promise: IThenable<T>, initialValue: T = undefined, modifier =  IDENTITY): IPromiseBasedObservable<T> {
