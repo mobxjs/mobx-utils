bfcd1116bb6ffad9ecf66df2307b5becfb6ca542
Michel Weststrate <mweststrate@gmail.com>
2016-08-15 21:06:03 +0200

    Introduced `createViewModel`

---
+++ src/create-view-model.ts
@@ -0,0 +1,93 @@
+import {observable, action, ObservableMap, asMap, isObservableObject} from "mobx";
+import {invariant} from "./utils";
+
+export interface IViewModel<T> {
+    model: T;
+    reset(): void;
+    submit(): void;
+    isDirty: boolean;
+    isPropertyDirty(key: string): boolean;
+}
+
+const RESERVED_NAMES = ["model", "reset", "submit", "isDirty", "isPropertyDirty"];
+
+class ViewModel<T> implements IViewModel<T> {
+    @observable isDirty = false;
+    localValues: ObservableMap<any> = asMap({});
+    dirtyMap: ObservableMap<any> = asMap({});
+
+    constructor(public model: T) {
+        invariant(isObservableObject(model), "createViewModel expects an observable object");
+        Object.keys(model).forEach(key => {
+            invariant(RESERVED_NAMES.indexOf(key) === -1, `The propertyname ${key} is reserved and cannot be used with viewModels`);
+            // TODO: create shallow clones for arrays and maps and observe those
+            Object.defineProperty(this, key, {
+                enumerable: true,
+                configurable: true,
+                get: () => {
+                    if (this.isPropertyDirty(key))
+                        return this.localValues.get(key);
+                    else
+                        return (this.model as any)[key];
+                },
+                set: action((value: any) => {
+                    if (this.isPropertyDirty(key) || value !== (this.model as any)[key]) {
+                        this.isDirty = true;
+                        this.dirtyMap.set(key, true);
+                        this.localValues.set(key, value);
+                    }
+                })
+            });
+        });
+    }
+
+    isPropertyDirty(key: string): boolean {
+        return this.dirtyMap.get(key) === true;
+    }
+
+    @action submit() {
+        if (this.isDirty) {
+            this.isDirty = false;
+            this.dirtyMap.entries().forEach(([key, dirty]) => {
+                if (dirty === true) {
+                    (this.model as any)[key] = this.localValues.get(key);
+                    this.dirtyMap.set(key, false);
+                    this.localValues.delete(key);
+                }
+            });
+        }
+    }
+
+    @action reset() {
+        if (this.isDirty) {
+            this.isDirty = false;
+            this.dirtyMap.entries().forEach(([key, dirty]) => {
+                if (dirty === true) {
+                    this.dirtyMap.set(key, false);
+                    this.localValues.delete(key);
+                }
+            });
+        }
+    }
+}
+
+/**
+ * `createViewModel` takes an object with observable properties (model)
+ * and wraps a view model around it. The view model proxies all enumerable property of the original model with the following behavior:
+ *  - as long as no new value has been assigned to the viewmodel property, the original property will be returned, and any future change in the model will be visible in the view model as well
+ *  - once a new value has been assigned to a property of the viewmodel, that value will be returned during a read of that property in the future
+ *
+ * The viewmodel exposes the following additional methods, besides all the enumerable properties of the model:
+ * - `submit()`: copies all the values of the viewmodel to the model and resets the state
+ * - `reset()`: resets the state of the view model, abandoning all local modificatoins
+ * - `isDirty`: observable property indicating if the viewModel contains any modifications
+ * - `isPropertyDirty(propName)`: returns true if the specified property is dirty
+ *
+ * N.B. doesn't support observable arrays and maps yet
+ *
+ * @param {T} model
+ * @returns {(T & IViewModel<T>)}
+ */
+export function createViewModel<T>(model: T): T & IViewModel<T> {
+    return new ViewModel(model) as any;
+}
