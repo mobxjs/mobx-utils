9bb78ee9d6d061ab3552da4d9b033c91f3590c0a
Michel Weststrate <mweststrate@gmail.com>
2016-08-15 12:11:17 +0200

    Test and docs for queueProcessor

--- src/queue-processor.ts
+++ src/queue-processor.ts
@@ -1,13 +1,29 @@
 import {isAction, autorun, autorunAsync, action, isObservableArray} from "mobx";
 import {IDisposer} from "./utils";
 
+/**
+ * `queueProcessor` takes an observable array, observes it and calls `processor`
+ * for each new item added to the observable array, optionally deboucing the action
+ *
+ * @param {T[]} observableArray observable array instance to track
+ * @param {(item: T) => void} processor action to call per item
+ * @param {number} [debounce=0] optional debounce time in ms. With debounce 0 the processor will run synchronously
+ * @returns {IDisposer} stops the processor
+ */
 export function queueProcessor<T>(observableArray: T[], processor: (item: T) => void, debounce = 0): IDisposer {
     if (!isObservableArray(observableArray))
         throw new Error("Expected observable array as first argument");
     if (!isAction(processor))
         processor = action("queueProcessor", processor);
 
-    const runner = () => observableArray.splice(0).forEach(processor);
+    const runner = () => {
+        // construct a final set
+        const items = observableArray.slice(0);
+        // clear the queue for next iteration
+        observableArray.splice(0);
+        // fire processor
+        items.forEach(processor);
+    };
     if (debounce >  0)
         return autorunAsync(runner, debounce);
     else
---
+++ test/queue-processor.js
@@ -0,0 +1,66 @@
+"use strict";
+
+const utils = require("../");
+const mobx = require("mobx");
+const test = require("tape");
+
+test("sync processor should work", t => {
+    const q = mobx.observable([1, 2])
+    const res = [];
+
+    const stop = utils.queueProcessor(q, v => res.push(v * 2))
+
+    t.deepEqual(res, [2, 4])
+    t.equal(q.length, 0)
+
+    q.push(3)
+    t.deepEqual(res, [2, 4, 6])
+
+    q.push(4, 5)
+    t.equal(q.length, 0)
+    t.deepEqual(res, [2, 4, 6, 8, 10])
+
+    mobx.transaction(() => {
+        q.unshift(6, 7)
+        t.equal(q.length, 2)
+        t.deepEqual(res, [2, 4, 6, 8, 10])
+    })
+
+    t.equal(q.length, 0)
+    t.deepEqual(res, [2, 4, 6, 8, 10, 12, 14])
+
+    stop()
+    q.push(42)
+    t.equal(q.length, 1)
+    t.deepEqual(res, [2, 4, 6, 8, 10, 12, 14])
+
+    t.end();
+})
+
+test("async processor should work", t => {
+    const q = mobx.observable([1, 2])
+    const res = [];
+
+    const stop = utils.queueProcessor(q, v => res.push(v * 2), 10)
+
+    t.equal(res.length, 0)
+    t.equal(q.length, 2)
+
+    setTimeout(() => {
+        t.deepEqual(res, [2, 4])
+        t.equal(q.length, 0)
+
+        q.push(3)
+        t.equal(q.length, 1)
+        t.deepEqual(res, [2, 4])
+
+        setTimeout(() => {
+            t.equal(q.length, 0)
+            t.deepEqual(res, [2, 4, 6])
+
+            stop()
+            t.end()
+        }, 50)
+    }, 50)
+
+})
