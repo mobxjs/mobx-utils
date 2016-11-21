5f31cf1abe386bd7d3580324d2dd1d6a5d54ec4a
jamiewinder <Jamiewinder@gmail.com>
2016-11-21 22:23:37 +0000

    add example

--- src/throttled-value.ts
+++ src/throttled-value.ts
@@ -18,8 +18,31 @@ export interface ThrottledValue<T> {
 
 /**
  * `throttledValue` creates an observable object which has two versions of its value; the 'latest' value that was applied,
- * and a 'current' value that eventually matches the latest, but is throttled by the specified amount. This is useful when
- * the value can change rapidly, but you only want to 'apply' the value in a throttled manner.
+ * and a 'current' value that is eventually synchronised with the latest, but is throttled by the specified amount. This 
+ * is useful when the value can change rapidly, but you only want to 'apply' the value in a throttled manner.
+ *
+ * @example
+ * const FilteredList = observer(({ filter, items }) => {
+ *     // use the 'current' value as the actual filter value
+ *     const filteredItems = items
+ *        .filter((item) => item.indexOf(filter.current) !== -1)
+ *        .map((item) => <li key={item}>{item}</li>);
+ *  
+ *     return (
+ *         <div>
+ *             // use the 'latest' value as the filter input value
+ *             <input type='text' value={filter.latest} onInput={(ev) => filter.set(ev.currentTarget.value)} />
+ *             <ul>
+ *                 {filteredItems}
+ *             </ul>
+ *         </div>
+ *     );
+ * });
+ *
+ * // create the filter as a throttledValue which is throttled to synchronous at most once per second
+ * const filter = throttledValue('', 1000);
+ * const items = ['one', 'two', 'three', 'four', 'five', 'six'];
+ * ReactDOM.render(<FilteredList filter={filter} items={items} />, document.getElementById('host'));
  *
  * @param {T} [initialValue} the initial value
  * @param {T} [delay] the throttle delay for applying the latest value to the current value
