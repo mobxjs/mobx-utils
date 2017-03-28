3d094bcd043d952952fd22ff0955543a4d4f477a
Azat Razetdinov <razetdinov@gmail.com>
2017-03-28 09:26:51 +0300

    Docs fix

--- src/lazy-observable.ts
+++ src/lazy-observable.ts
@@ -3,7 +3,7 @@ import {observable, extras, action} from "mobx";
 
 /**
  * `lazyObservable` creates an observable around a `fetch` method that will not be invoked
- * util the observable is needed the first time.
+ * until the observable is needed the first time.
  * The fetch method receives a `sink` callback which can be used to replace the
  * current value of the lazyObservable. It is allowed to call `sink` multiple times
  * to keep the lazyObservable up to date with some external resource.
