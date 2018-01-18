c9f573714528f83de1d3eaed15b6dec7883127e0
liangminjian@itouchtv.cn <liangminjian@itouchtv.cn>
2018-01-18 15:15:51 +0800

    feat(async-actions): add asyncActionWithCancel feature

--- test/async-action.ts
+++ test/async-action.ts
@@ -214,6 +214,125 @@ test("it should support logging", t => {
     }, 10)
 })
 
+test("it should support asyncActionWithCancel(cb, type) as decorator", t => {
+    const values: any[] = []
+
+    mobx.useStrict(true)
+
+    class X {
+        @mobx.observable a = 1;
+
+        @utils.asyncActionWithCancel(() => {}, 'takeLatest')
+        *f(initial: number) {
+            this.a = initial // this runs in action
+            try {
+                this.a = yield delay(100, 5, true) // and this as well!
+                yield delay(100, 0)
+                this.a = 4
+            } catch (e) {
+                this.a = e
+            }
+            return this.a
+        }
+    }
+
+    const x = new X()
+    mobx.reaction(() => x.a, v => values.push(v), { fireImmediately: true })
+
+    setTimeout(() => {
+        // TODO: mweh on any cast...
+        ;(x.f(/*test binding*/ 2) as any).then((v: number) => {
+            // note: ideally, type of v should be inferred..
+            t.is(v, 5)
+            t.deepEqual(values, [1, 2, 5])
+            t.is(x.a, 5) // correct instance modified?
+            t.end()
+        })
+    }, 10)
+})
+
+test("it should support cancel prev the same kinds of async generator actions in 'takeLatest' type", t => {
+    mobx.useStrict(true)
+    const values: any[] = []
+    const x = mobx.observable({ a: 1 })
+    mobx.reaction(() => x.a, v => values.push(v), { fireImmediately: true })
+
+    const f = utils.asyncActionWithCancel(() => {}, 'takeLatest')(function*(initial: number) {
+        x.a = initial // this runs in action
+        x.a = yield delay(100, 3) // and this as well!
+        yield delay(100, 0)
+        x.a = 4
+        return x.a
+    })
+
+    setTimeout(() => {
+        f(2)
+        f(3)
+        f(5).then((v: number) => {
+            // note: ideally, type of v should be inferred..
+            t.is(v, 4)
+            t.deepEqual(values, [1, 2, 3, 5, 3, 4])
+            t.end()
+        })
+    }, 10)
+})
+
+test("it should support cancel current async generator actions in 'takeLatest' type", t => {
+    mobx.useStrict(true)
+    const values: any[] = []
+    const x = mobx.observable({ a: 1 })
+    mobx.reaction(() => x.a, v => values.push(v), { fireImmediately: true })
+
+    let canceler
+    const f = utils.asyncActionWithCancel(cancel => canceler = cancel, 'takeLatest')(function*(initial: number) {
+        x.a = initial // this runs in action
+        x.a = yield delay(100, 3) // and this as well!
+        yield delay(100, 0)
+        x.a = 4
+        return x.a
+    })
+
+    setTimeout(() => {
+        f(2)
+        f(3)
+        f(5)
+        canceler()
+        setTimeout(() => {
+            t.is(x.a, 5)
+            t.deepEqual(values, [1, 2, 3, 5])
+            t.end()
+        }, 10)
+    }, 10)
+})
+
+test("it should support cancel all the same kinds of async generator actions in 'takeEvery' type", t => {
+    mobx.useStrict(true)
+    const values: any[] = []
+    const x = mobx.observable({ a: 1 })
+    mobx.reaction(() => x.a, v => values.push(v), { fireImmediately: true })
+
+    let canceler
+    const f = utils.asyncActionWithCancel(cancel => canceler = cancel, 'takeLatest')(function*(initial: number) {
+        x.a = initial // this runs in action
+        x.a = yield delay(100, 3) // and this as well!
+        yield delay(100, 0)
+        x.a = 4
+        return x.a
+    })
+
+    setTimeout(() => {
+        f(2)
+        f(3)
+        f(5)
+        canceler()
+        setTimeout(() => {
+            t.is(x.a, 5)
+            t.deepEqual(values, [1, 2, 3, 5])
+            t.end()
+        }, 10)
+    }, 10)
+})
+
 function stripEvents(events) {
     return events.map(e => {
         delete e.object
