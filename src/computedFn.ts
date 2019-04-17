37145b2c33f3ea0b79ce379072eee7c5992a17e4
Michel Weststrate <mweststrate@gmail.com>
2019-04-18 00:03:53 +0200

    Implemented computedFn

--- src/computedFn.ts
+++ src/computedFn.ts
@@ -1,4 +1,23 @@
+import { DeepMap } from "./deepMap"
+import { IComputedValue, computed, onBecomeUnobserved } from "mobx";
 
-export function bla() {
+export function computedFn<T extends Function>(fn: T, autoCleanup = true) {
+  const d = new DeepMap<IComputedValue<any>>()
 
+  return function(...args: any[]) {
+    const self = this
+    const entry = d.entry(args)
+    if (entry.exists())
+      return entry.get().get()
+
+    // create new entry
+    const c = computed(() => {
+      return fn.apply(self, args)
+    })
+    if (!autoCleanup) onBecomeUnobserved(c, () => {
+      d.entry(args).delete()
+    })
+    entry.set(c)
+    return c.get()
+  }
 }
