fb6c8aff9218ef7ee85d79f94292f4e37bab6cb3
Joost Reuzel <joost_reuzel@hotmail.com>
2020-12-11 01:27:47 +0100

    Adding toPromise

---
+++ src/toPromise.ts
@@ -0,0 +1,52 @@
+import { autorun, computed } from "mobx"
+
+/**
+ * Creates a Promise that resolves with an observed value when fulfills the conditions of a predicate.
+ *
+ * The generator function selects the to be observed value. If the generator creates an undefined value it is ignored.
+ * If the generator creates a value, it will be passed to the predicate function that indicates if that value should
+ * resolve the promise.
+ *
+ * Any error in the generator or predicate functions will reject the promise.
+ *
+ * An abort signal may be provided to cancel the wait for the observed value. This will reject the promise.
+ *
+ * @example
+ * const store = observed({
+ *   foo:""
+ * });
+ *
+ * //wait until store.foo contains the character "b"
+ * await toPromise(()=>store.foo, (value)=>value.indexOf("b")>=0);
+ *  *
+ * @param generator - selects the value to be observed
+ * @param predicate - optional predicate to indicate when the observed value should resolve the promise. By default accepts all values.
+ * @param abortSignal - singal to abort wait for the observed value change (using AbortSignal WebAPI)
+ * @returns a Promise that resolves with the observed value when fulfills the conditions of the predicate
+ */
+export function toPromise<T>(
+    generator: () => T | undefined,
+    predicate: (value: T) => boolean = () => true,
+    abortSignal?: AbortSignal
+): Promise<T> {
+    return new Promise<T>((resolve, reject) => {
+        const disposer = autorun((reaction) => {
+            try {
+                const value = computed(generator).get()
+                if (value !== undefined && predicate(value)) {
+                    resolve(value)
+                    reaction.dispose()
+                }
+            } catch (err) {
+                reject(err)
+                reaction.dispose()
+            }
+        })
+        if (abortSignal) {
+            abortSignal.onabort = () => {
+                disposer()
+                reject(new Error("Aborted wait for observed value change"))
+            }
+        }
+    })
+}
---
+++ test/toPromise.ts
@@ -0,0 +1,113 @@
+import { observable, runInAction } from "mobx"
+import { toPromise } from "../src/toPromise"
+
+test("Should wait for value", async () => {
+    const store = observable({ foo: "" })
+
+    const promise = toPromise(
+        () => store.foo,
+        (foo) => foo.indexOf("b") >= 0
+    )
+    setImmediate(() => {
+        runInAction(() => {
+            store.foo = "bar"
+        })
+    })
+
+    return expect(promise).resolves.toEqual("bar")
+})
+
+test("Should ignore non matching value", async () => {
+    const store = observable({ foo: "" })
+
+    const promise = toPromise(
+        () => store.foo,
+        (foo) => foo.indexOf("b") >= 0
+    )
+    setImmediate(() => {
+        runInAction(() => {
+            store.foo = "hello"
+        })
+
+        runInAction(() => {
+            store.foo = "bar"
+        })
+    })
+
+    return expect(promise).resolves.toEqual("bar")
+})
+
+test("Should resolve when value becomes available", async () => {
+    const store = observable({ foo: "", bar: undefined })
+
+    const promise = toPromise(() => store.bar)
+    setImmediate(() => {
+        runInAction(() => {
+            store.bar = undefined
+        })
+
+        runInAction(() => {
+            store.bar = "hello"
+        })
+    })
+
+    return expect(promise).resolves.toEqual("hello")
+})
+
+test("Should resolve when item gets added to map", async () => {
+    const store = observable(new Map<string, string>())
+
+    const promise = toPromise(() => store.get("but"))
+    setImmediate(() => {
+        runInAction(() => {
+            store.set("no no", "not this one")
+        })
+
+        runInAction(() => {
+            store.set("but", "this one")
+        })
+    })
+
+    return expect(promise).resolves.toEqual("this one")
+})
+
+test("Should reject when generator throws error", async () => {
+    const error = new Error("Yikes!")
+
+    const promise = toPromise(() => {
+        throw error
+    })
+
+    return expect(promise).rejects.toEqual(error)
+})
+
+test("Should reject when predicate throws error", async () => {
+    const store = observable({ foo: "bar" })
+
+    const error = new Error("Yikes!")
+
+    const promise = toPromise(
+        () => store.foo,
+        (value) => {
+            throw error
+        }
+    )
+
+    return expect(promise).rejects.toEqual(error)
+})
+
+test("Should reject when aborted", async () => {
+    const store = observable({ foo: "bar" })
+
+    const aborter = new AbortController()
+
+    const promise = toPromise(
+        () => store.foo,
+        (value) => value == "hello",
+        aborter.signal
+    )
+
+    aborter.abort()
+
+    return expect(promise).rejects.toEqual(new Error("Aborted wait for observed value change"))
+})
