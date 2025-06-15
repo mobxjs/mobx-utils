0ab93b3c7d417a2b36463b2a0a82ab75c6175145
Wemyss <sam.w@canva.com>
2025-06-16 06:27:04 +1000

    fix: Update warnings for computedFn and createTransformer to respect requiresReaction flags (#337)
    * .
    
    * .
    
    * .
    
    * .

--- src/computedFn.ts
+++ src/computedFn.ts
@@ -67,7 +67,10 @@ export function computedFn<T extends (...args: any[]) => any>(
         if (entry.exists()) return entry.get().get()
         // if function is invoked, and its a cache miss without reactive, there is no point in caching...
         if (!opts.keepAlive && !_isComputingDerivation()) {
-            if (!memoWarned && _getGlobalState().computedRequiresReaction) {
+            if (
+                !memoWarned &&
+                (opts.requiresReaction ?? _getGlobalState().computedRequiresReaction)
+            ) {
                 console.warn(
                     "Invoking a computedFn from outside a reactive context won't be memoized " +
                         "and is cleaned up immediately, unless keepAlive is set."
